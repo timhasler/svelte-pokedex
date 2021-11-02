2e3da6c5700a3f695a85914bf0f32dcd8b955c5d
tim.hasler <tim.hasler@itech.media>
2021-11-02 18:56:35 +0000

    layout and navbar

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Navbar from '../components/navbar/Navbar.svelte'
+</script>
+<svelte:head>
+    <title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<Navbar />
+<slot></slot>
---
+++ src/routes/about.svelte
@@ -0,0 +1,1 @@
+<p>This is a tester project to learn svelte</p>
