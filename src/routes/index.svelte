16977c54730c93d0afe9cd8ce6fbb7393d80c93d
tim.hasler <tim.hasler@itech.media>
2021-11-03 23:46:58 +0000

    add search

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,13 +14,25 @@
 
 <script>
     export let pokemon = [];
+    let searchTerm = "";
+    let filteredPokemon;
+
+    $: {
+        if (searchTerm) {
+            filteredPokemon = pokemon.filter((pokeman) => {
+                return pokeman.name.toLowerCase().includes(searchTerm.toLowerCase());
+            })
+        } else {
+            filteredPokemon = [...pokemon]
+        }
+    }
 </script>
 
 <h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit Pokedex</h1>
 <input class="rounded-md w-full text-lg p-4 border-2
-    border-gray-200" type="text" placeholder="Search for Pokemon">
+    border-gray-200" type="text" placeholder="Search for Pokemon" bind:value={searchTerm}>
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-    {#each pokemon as pokeman}
+    {#each filteredPokemon as pokeman}
         <PokeCard pokeman={pokeman} />
     {/each}
 </div>
