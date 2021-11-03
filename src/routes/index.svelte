eb32b383162de5eac5dc495ff3e27f4f4beac701
tim.hasler <tim.hasler@itech.media>
2021-11-03 01:13:21 +0000

    static generation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,8 +5,9 @@
 </script>
 
 <h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit Pokedex</h1>
-
-<div class="grid gap-4 md:grid-cols-2 grid-cols-1">
+<input class="rounded-md w-full text-lg p-4 border-2
+    border-gray-200" type="text" placeholder="Search for Pokemon">
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
     {#each $pokemon as pokeman}
         <PokeCard pokeman={pokeman} />
     {/each}
