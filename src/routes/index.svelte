261e2c5ed4e40bdf5334eb620616002bc308fcc0
Tim Hasler <timhasler@itech.media>
2021-11-05 23:31:56 +0000

    tidy data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,11 @@
 <script context="module">
     import PokeCard from '../components/pokeCard/PokeCard.svelte';
+    import { getPokemon } from '../data/pokemon.js';
 
     export async function load({ fetch }) {
-        const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
-        const res = await fetch(url);
-        const data = await res.json();
-        const pokemon = data.results.map((data, index) => ({
-            name: data.name,
-            id: index + 1,
-            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                index + 1
-            }.png`
-        }));
-
         return {
             props: {
-                pokemon
+                pokemon: await getPokemon(fetch)
             }
         }
     }
