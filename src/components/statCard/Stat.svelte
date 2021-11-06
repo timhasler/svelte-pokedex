3f0ab954dd270b287885df1d67c0284e9aa10e6c
Tim Hasler <timhasler@itech.media>
2021-11-06 16:41:54 +0000

    local serve and stat formatting

--- src/components/statCard/Stat.svelte
+++ src/components/statCard/Stat.svelte
@@ -2,4 +2,4 @@
     export let stat;
 </script>
 
-<h3 class="uppercase text-2xl text-left">{stat.name}:</h3><h3 class="uppercase text-2xl text-middle">{stat.value}</h3>
+<h3 class="uppercase text-2xl">{stat.name}: {stat.value}</h3>
